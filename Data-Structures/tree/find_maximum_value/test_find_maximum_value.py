af6b90419d410b10acc93aae78d4fb012e8d7660
Leandro Rodriguez <lrodriguezjb@gmail.com>
2020-01-01 23:33:39 -0800

    added function

---
+++ Data-Structures/tree/find_maximum_value/test_find_maximum_value.py
@@ -0,0 +1,66 @@
+import pytest
+from find_maximum_value import BinaryTree
+
+def test_add_one_to_tree():
+    new_tree = BinaryTree()
+    new_tree.add(2)
+    assert new_tree.root.value == 2
+
+def test_add_three_to_tree():
+    new_tree = BinaryTree()
+    new_tree.add(2)
+    assert new_tree.root.value == 2
+    new_tree.add(7)
+    assert new_tree.root.left.value == 7
+    new_tree.add(5)
+    assert new_tree.root.right.value == 5
+
+def test_breadth_first_three():
+    new_tree, expected = BinaryTree(), [2,7,5]
+    new_tree.add(2)
+    new_tree.add(7)
+    new_tree.add(5)
+    assert new_tree.breadth_first() == expected
+
+def test_breadth_first_nine():
+    new_tree = BinaryTree()
+    expected = [2,7,5,2,6,9,5,11,4]
+    new_tree.add(2)
+    new_tree.add(7)
+    new_tree.add(5)
+    new_tree.add(2)
+    new_tree.add(6)
+    new_tree.add(9)
+    new_tree.add(5)
+    new_tree.add(11)
+    new_tree.add(4)
+    assert new_tree.breadth_first() == expected
+
+def test_maximum_value():
+    new_tree = BinaryTree()
+    expected = 11
+    new_tree.add(2)
+    new_tree.add(7)
+    new_tree.add(5)
+    new_tree.add(2)
+    new_tree.add(6)
+    new_tree.add(9)
+    new_tree.add(5)
+    new_tree.add(11)
+    new_tree.add(4)
+    assert new_tree.find_maximum_value() == expected
+
+def test_negative_max_value():
+    new_tree = BinaryTree()
+    expected = -2
+    new_tree.add(-2)
+    new_tree.add(-3)
+    new_tree.add(-7)
+    new_tree.add(-5)
+    new_tree.add(-2)
+    new_tree.add(-6)
+    new_tree.add(-9)
+    new_tree.add(-5)
+    new_tree.add(-11)
+    new_tree.add(-4)
+    assert new_tree.find_maximum_value() == expected
